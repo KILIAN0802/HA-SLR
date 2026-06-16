0df2f5c41e1cfcaa29a7a127d3e1538082701750
nguyendat <nguyendat08022004@gmail.com>
2026-06-16 16:24:59 +0700

    fusion-gate4

--- Code/Network/SL_GCN/run_ultimate_400vsl.sh
+++ Code/Network/SL_GCN/run_ultimate_400vsl.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Chỉ định GPU chạy (Ví dụ: 0, 1, 2...). Nếu GPU 0 bị đầy, hãy đổi thành số GPU trống khác.
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 # Lấy ngày tháng năm và giờ phút giây để tạo thư mục lưu log riêng biệt
 LOG_DATE=$(date +%d-%m-%Y)
